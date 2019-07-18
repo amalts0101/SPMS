be0ef2d4bab98a4f884e86d7436d6bfb0efba492
Amal T S <amalts0101@gmail.com>
2019-07-18 18:18:24 +0530

    # 25572669 - Login - Added user authentication.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'employees/index'
+  devise_for :users
+  get 'users/index'
   get 'activities/index'
   get 'projects/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
