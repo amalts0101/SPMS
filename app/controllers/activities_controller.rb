b712d5ceed4e885556d9cea0054899e3a8f3b3ba
joicy george <joicygeorge510@gmail.com>
2019-07-19 16:45:43 +0530

    #25572741-Add Activity-Added activity adding functionality

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,4 +1,17 @@
 class ActivitiesController < ApplicationController
   def index
   end
+
+  def create
+    @activity = current_user.activities.new(activity_params)
+    @activity.project_id = 1
+    @activity.save
+    redirect_to root_path
+  end
+
+ 
+private
+  def activity_params
+    params.require(:activity).permit(:name, :hours, :project_id)
+  end
 end
