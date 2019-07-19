269f0dcc18008e3de9f3c357ccbb7c6eae6ba8fc
Amal T S <amalts0101@gmail.com>
2019-07-19 09:36:45 +0530

    # 25572669 - Login - Removed user's index page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'users/index'
   get 'activities/index'
   get 'projects/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
