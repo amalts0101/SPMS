603d4815bbe070a18528c2e595a341b93bdf9422
joicy george <joicygeorge510@gmail.com>
2019-07-25 14:04:14 +0530

    #25572733-Activity list - Alignment fixed and add activity updated

--- app/models/concerns/user/activity_manager.rb
+++ app/models/concerns/user/activity_manager.rb
@@ -2,9 +2,9 @@ module User::ActivityManager
   extend ActiveSupport::Concern
 
   def create_activity(params)
-  	activity = self.activities.new(params)
-  	activity.date = Date.strptime(params[:date], "%B %d, %Y")
-  	activity.project_id = params[:project_id].map(&:presence).compact.first
+    activity = self.activities.new(params)
+    activity.date = Date.strptime(params[:date], "%B %d, %Y")
+    activity.project_id = params[:project_id].map(&:presence).compact.first
     activity.save
     activity
   end
