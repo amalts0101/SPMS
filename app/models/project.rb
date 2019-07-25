d0743993faa040403c57adc251d02eb405dc7ba5
namitha <namithachandran003@gmail.com>
2019-07-25 14:32:41 +0530

    # 25572881 - Dashboard - Added total hours.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,10 @@
 class Project < ApplicationRecord
   has_many :activities
+  has_many :user_projects
   has_many :users, through: :user_projects
   mount_uploader :logo, LogoUploader
   enum status: { created: 0, completed: 1}
+
   def total_hours
 	self.activities.sum(:hours).to_f
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :user_projects
   has_many :projects, through: :user_projects
   has_many :activities
+
+  def total_activity_hours.
+	self.activities.sum(:hours).to_f
+  end
 end
