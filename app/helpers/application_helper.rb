29c30a91ecb40428e2e8444e5dc8a236ba370e42
Amal T S <amalts0101@gmail.com>
2019-07-25 18:54:16 +0530

    # 25572669 - Login - Integrated new alerts and moved charts into partials.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+
+  def toast_title(status)
+    case status
+    when 'alert'
+      'Warning'
+    when 'error'
+      'Error'
+    when 'success'
+      'Success'
+    when 'info'
+      'Information'
+    end
+  end
+
+  def toast_icon(status)
+    case status
+    when 'alert'
+      'warning'
+    when 'error'
+      'error'
+    when 'success'
+      'success'
+    when 'info'
+      'info'
+    end
+  end
 end
