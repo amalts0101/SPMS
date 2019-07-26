5779be9c8a0f827e7cd03caafa66ce2933859e33
Amal T S <amalts0101@gmail.com>
2019-07-26 12:38:17 +0530

    # 25577916 - Setup Admin Panel - Added administration for Projects and Users.

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,23 +4,39 @@ ActiveAdmin.register_page "Dashboard" do
   content title: proc { I18n.t("active_admin.dashboard") } do
     div class: "blank_slate_container", id: "dashboard_default_message" do
       span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+        span ("Welcome to SPMS | Admin Panel")
       end
     end
 
+
     # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    section status_tag 'Projects' do
+      columns do
+        column do
+          panel "Recent Projects" do
+            ul do
+              Project.last(5).map do |project|
+                li link_to(project.name, admin_project_path(project))
+              end
+            end
+          end
+        end
+      end
+    end
+
+    section status_tag 'Users'do
+      columns do
+        column do
+          panel "Recent Users" do
+            ul do
+              User.last(5).map do |user|
+                li link_to(user.name, admin_user_path(user))
+              end
+            end
+          end
+        end
+      end
+    end
 
     #   column do
     #     panel "Info" do
--- app/models/project.rb
+++ app/models/project.rb
@@ -4,8 +4,9 @@ class Project < ApplicationRecord
   has_many :users, through: :user_projects
   mount_uploader :logo, LogoUploader
   enum status: { created: 0, completed: 1}
+  accepts_nested_attributes_for :users, :allow_destroy => true
 
   def total_hours
-	self.activities.sum(:hours).to_f
+	 self.activities.sum(:hours).to_f
   end
 end
